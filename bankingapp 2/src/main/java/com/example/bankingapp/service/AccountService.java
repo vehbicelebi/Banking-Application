4fba7469d9e67b59f90e445c18d4752837e8d28a
Vehbi Celebi <vehbi.celebi@stud.hn.de>
2024-04-20 20:40:05 +0200

    finished banking app

---
+++ bankingapp 2/src/main/java/com/example/bankingapp/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.example.bankingapp.service;
+
+import com.example.bankingapp.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto);
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id, double amount);
+    AccountDto withdraw(Long id, double amount);
+    List<AccountDto> getAllAccounts();
+    void deleteAccount(Long id);
+
+}
